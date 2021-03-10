052a2fb6909b5bebb8b9e6e6cd35abc4da97de07
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-03-10 21:50:41 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (!str.length || !bracketsConfig.length) {
+    console.log("1")
+    return false;
+  } 
+  if (str.length % 2) {
+    console.log("2")
+    return false
+  }
+  let strToClear = str;
+  const regexps = bracketsConfig.map(bracketsPair => {
+    if(!isNaN(+bracketsPair[0] && !isNaN(+bracketsPair[1]))) {
+        let regexp = new RegExp(`${bracketsPair[0]}${bracketsPair[1]}`, '');
+        return regexp; 
+    }
+    let regexp = new RegExp(`\\${bracketsPair[0]}\\${bracketsPair[1]}`, '');
+    return regexp;
+  })
+  console.log(regexps)
+  let mark = true;
+  while(mark) {
+    mark = strToClear.length;
+    for(let i = 0; i < regexps.length; i++) {
+      if(!strToClear.match(regexps[i])) {
+        console.log("3")
+        mark = false;
+      }
+      if(strToClear.match(regexps[i])) {
+        strToClear = strToClear.replace(regexps[i], "")
+        mark = true;
+        break;
+      }
+    }
+  }
+  return !strToClear.length
 }
